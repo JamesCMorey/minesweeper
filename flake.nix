1f2aa10c87b7966ee02f83d33727a17ca4dd71b2
James C. Morey <jmorey551@gmail.com>
2025-04-25 14:26:12 -0500

    Add flake for easy development

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "Minesweeper with ninja build system";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        libraries = [ pkgs.SDL2 pkgs.SDL2_ttf];
+      in {
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.gcc
+            pkgs.pkg-config
+            pkgs.cmake
+            pkgs.ninja
+            pkgs.meson
+            pkgs.gdb
+            pkgs.valgrind
+          ];
+
+          buildInputs = libraries;
+        };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+        pname = "minesweeper";
+        version = "0.1";
+        src = ./.;
+
+        nativeBuildInputs = [
+          pkgs.pkg-config
+          pkgs.ninja
+          pkgs.meson
+        ];
+
+        buildInputs = libraries;
+
+        buildPhase = ''
+          mkdir -p build
+          cd build
+          meson setup $src
+          ninja
+          '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp sweeper $out/bin
+          '';
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/sweeper";
+        };
+      }
+    );
+}
